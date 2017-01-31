12712bafc458434bcbeb7023642e2c6bc91a109f
sub77 <sub77@ymail.com>
2017-01-31 17:02:22 +0100

    msm8226-common: gps: Update for N

--- msm8226.mk
+++ msm8226.mk
@@ -104,6 +104,10 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# GPS
+PRODUCT_PACKAGES += \
+    gps.msm8226
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
