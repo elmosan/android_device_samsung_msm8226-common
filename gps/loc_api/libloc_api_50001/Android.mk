f0378d5f6469bddaa23aff359592b87c4189c10f
sub77 <sub77@ymail.com>
2017-01-31 22:00:04 +0000

    msm8226-common: gps: Fix
    Change-Id: I8ebae5c99230dc3de0b53fd739986c3204981d5b

--- gps/loc_api/libloc_api_50001/Android.mk
+++ gps/loc_api/libloc_api_50001/Android.mk
@@ -41,7 +41,7 @@ LOCAL_CFLAGS += \
 LOCAL_C_INCLUDES:= \
     $(TARGET_OUT_HEADERS)/gps.utils \
     $(TARGET_OUT_HEADERS)/libloc_core \
-    device/samsung/matissewifi/gps/loc_api/libloc_api_50001
+    device/samsung/msm8226-common/gps/loc_api/libloc_api_50001
 
 LOCAL_COPY_HEADERS_TO:= libloc_eng/
 LOCAL_COPY_HEADERS:= \
